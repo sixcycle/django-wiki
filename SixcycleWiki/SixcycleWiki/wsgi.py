16ed5aa088facd567ee6b1c294719c241bee82d4
Ian <oboyle.ian@gmail.com>
2019-02-19 16:29:20 -0500

    Work

--- SixcycleWiki/SixcycleWiki/wsgi.py
+++ SixcycleWiki/SixcycleWiki/wsgi.py
@@ -2,6 +2,6 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, '/opt/python/current/app')
+sys.path.append("/opt/python/current/app/SixcycleWiki")
 os.environ['DJANGO_SETTINGS_MODULE'] = 'SixcycleWiki.settings'
 application = get_wsgi_application()
