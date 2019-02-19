2406e775a82209ca4ffb04e4ea1c0cd8ebfb01d8
Ian <oboyle.ian@gmail.com>
2019-02-19 16:13:21 -0500

    work

--- SixcycleWiki/SixcycleWiki/wsgi.py
+++ SixcycleWiki/SixcycleWiki/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "SixcycleWiki.SixcycleWiki.settings"
+    "SixcycleWiki.settings"
 )
 
 application = get_wsgi_application()
