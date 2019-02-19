9006b45362807245cd2a15aa22ae525bdd93320f
Ian <oboyle.ian@gmail.com>
2019-02-19 16:20:10 -0500

    maybe making the settings module explicit will do the trick ?

--- SixcycleWiki/SixcycleWiki/wsgi.py
+++ SixcycleWiki/SixcycleWiki/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
-    "settings"
+    "SixcycleWiki.SixcycleWiki.settings"
 )
 
 application = get_wsgi_application()
