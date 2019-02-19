0c605b22dadc3896a0f6119a2edd95ac9a01ed32
Ian <oboyle.ian@gmail.com>
2019-02-19 16:27:19 -0500

    trying a custom wsgi script cause django dev server vs mod_wsgi is sketch

--- SixcycleWiki/SixcycleWiki/wsgi.py
+++ SixcycleWiki/SixcycleWiki/wsgi.py
@@ -1,19 +1,7 @@
-"""
-WSGI config for SixcycleWiki project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "SixcycleWiki.SixcycleWiki.settings"
-)
-
+sys.path.insert(0, '/opt/python/current/app')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'SixcycleWiki.settings'
 application = get_wsgi_application()
