c610b5d63a61f9bbbae60a4e271ccba1d0abae60
Ian <oboyle.ian@gmail.com>
2019-02-21 13:41:46 -0500

    work

--- SixcycleWiki/SixcycleWiki/settings.py
+++ SixcycleWiki/SixcycleWiki/settings.py
@@ -109,7 +109,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'SixcycleWiki.wsgi.application'
-AWS_STORAGE_BUCKET_NAME = 'sixcycle-wiki-storage'
+AWS_STORAGE_BUCKET_NAME = 'sixcycle-wiki-storage/'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
