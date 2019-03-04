dc4a90eb6958ab668e802a4f83e7650f799067c7
Ian <oboyle.ian@gmail.com>
2019-03-04 15:37:08 -0500

    Work

--- SixcycleWiki/SixcycleWiki/settings.py
+++ SixcycleWiki/SixcycleWiki/settings.py
@@ -31,6 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'sixcycle-wiki-env.7dwtp34aiu.us-east-1.elasticbeanstalk.com',
     'wiki-stg.sixcycle.com',
+    'dev2.sixcycle.com',
     '127.0.0.1'
     ]
 SITE_ID = 1
