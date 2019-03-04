40840fbde4ee92c68a1d88bf54fe2d1b94f432a1
Ian <oboyle.ian@gmail.com>
2019-03-04 12:45:05 -0500

    more changes to settings

--- SixcycleWiki/SixcycleWiki/settings.py
+++ SixcycleWiki/SixcycleWiki/settings.py
@@ -102,6 +102,10 @@ AUTHENTICATION_BACKENDS = [
 ]
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+    'sixcycle.com',
+    'localhost:8000',
+)
 
 
 STATIC_ROOT = 'static'
