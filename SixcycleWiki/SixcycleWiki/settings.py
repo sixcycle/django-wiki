752bf795ab0ad5d42e30928e295c39ca39e37459
Ian <oboyle.ian@gmail.com>
2019-03-04 14:32:46 -0500

    work

--- SixcycleWiki/SixcycleWiki/settings.py
+++ SixcycleWiki/SixcycleWiki/settings.py
@@ -101,11 +101,6 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_WHITELIST = (
-    'sixcycle.com',
-    'localhost:8000',
-)
 
 
 STATIC_ROOT = 'static'
