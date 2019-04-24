f94ab65a96a9aaffa8ae89e00aced2f3f2deee56
Ian <oboyle.ian@gmail.com>
2019-04-24 16:46:56 -0400

    Work

--- SixcycleWiki/SixcycleWiki/settings.py
+++ SixcycleWiki/SixcycleWiki/settings.py
@@ -268,6 +268,9 @@ def WIKI_CAN_READ(article, user):
             if user.organizationadmins_set.filter(organization=organization).exists():
                 return True
     except Exception as ex:
+        print("ex is {}").format(
+            ex
+        )
         return False
 
 
