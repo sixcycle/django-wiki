c31ea88a6d438d24a4272f162cc37508a3d3bb95
Ian <oboyle.ian@gmail.com>
2019-04-26 11:42:22 -0400

    work

--- SixcycleWiki/SixcycleWiki/settings.py
+++ SixcycleWiki/SixcycleWiki/settings.py
@@ -176,7 +176,8 @@ AWS_S3_REGION_NAME = 'us-east-1'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if ENVIRONMENT == "staging"
+
+if ENVIRONMENT == "staging":
     DB_HOST_URL = 'wiki.cd8omj5dryba.us-east-1.rds.amazonaws.com'
 elif ENVIRONMENT == "production":
     DB_HOST_URL = 'wiki.cd8omj5dryba.us-east-1.rds.amazonaws.com'
