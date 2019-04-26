5f72e6e09b59b7bb25c8c2c56477aee0ea5c753f
Ian <oboyle.ian@gmail.com>
2019-04-26 12:15:36 -0400

    work

--- SixcycleWiki/relationships/models.py
+++ SixcycleWiki/relationships/models.py
@@ -16,31 +16,42 @@ class BaseArticleRelationship(models.Model):
         abstract = True
 
 
-class OrganizationReadArticle(BaseArticleRelationship):
+class OrganizationArticle(BaseArticleRelationship):
+    # Represents an article that belongs to an organization,
+    organization = models.ForeignKey(Organization)
 
+
+class OrganizationReadArticle(BaseArticleRelationship):
+    # Read permissions for an entire organization on
+    # an article
     organization = models.ForeignKey(Organization)
 
 
 class OrganizationEditArticle(BaseArticleRelationship):
-
+    # Edit permissions for an entire organization on
+    # an article
     organization = models.ForeignKey(Organization)
 
 
 class GroupReadArticle(BaseArticleRelationship):
-
+    # Read permissions for an entire group on
+    # an article
     group = models.ForeignKey(Group)
 
 
 class GroupEditArticle(BaseArticleRelationship):
-
+    # Edit permissions for an entire group on
+    # an article
     group = models.ForeignKey(Group)
 
 
 class UserReadArticle(BaseArticleRelationship):
-
+    # Read permissions for a user on
+    # an article
     user = models.ForeignKey(User)
 
 
 class UserEditArticle(BaseArticleRelationship):
-
+    # Read permissions for a user on
+    # an article
     user = models.ForeignKey(User)
