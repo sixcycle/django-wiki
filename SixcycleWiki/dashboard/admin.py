013986f2b55c146a86bb219e753b1b4beb853537
Ian <oboyle.ian@gmail.com>
2019-04-26 12:55:25 -0400

    work

--- SixcycleWiki/dashboard/admin.py
+++ SixcycleWiki/dashboard/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from dashboard.models import OrganizationAdmins
+from dashboard.models import OrganizationAdmins, AllowedUsers
 # Register your models here.
 
 admin.site.register(OrganizationAdmins)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_files = ['user_email']
+
+admin.site.register(AllowedUsers, AllowedUsersAdmin)
