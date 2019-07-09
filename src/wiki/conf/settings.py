5bc775dd849ca928d1c198b1dc8aa240b84d7336
Waseem Hussain <w927hussain@gmail.com>
2019-07-09 10:46:19 -0400

    attempting to fix url path for retrieving S3 files

--- src/wiki/conf/settings.py
+++ src/wiki/conf/settings.py
@@ -336,6 +336,8 @@ STORAGE_BACKEND = getattr(
     'WIKI_STORAGE_BACKEND',
     default_storage)
 
+WIKI_ATTACHMENTS_STORAGE_BACKEND = 'storages.backends.s3boto3.S3Boto3Storage'
+
 #: Use django-sendfile for sending out files? Otherwise the whole file is
 #: first read into memory and than send with a mime type based on the file.
 USE_SENDFILE = getattr(django_settings, 'WIKI_ATTACHMENTS_USE_SENDFILE', False)
